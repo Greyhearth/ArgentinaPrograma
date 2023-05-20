53f006b9154c2e0d768e9ad2fe1b0feba17f9d2f
Gustavo <gustavo.grisetti@mi.unc.edu.ar>
2023-05-20 12:32:20 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,5 @@
+let base = 12.5;
+let altura = 5.3;
+console.log(base*altura) ;
+/* Si la base es 5 y la latura es 3, el área es 15 */
+/* Si la base es 12.5 y la latura es 5.3, el área es 66.25*/
---
+++ practico0/promedio3.js
@@ -0,0 +1,7 @@
+let num1 = -1.2;
+let num2 = 5.3;
+let num3 = -3.3;
+console.log((num1+num2+num3)/3);
+/* Con los valores 5, 7 y 9, el resultado será 7 */
+/* Con los valores -2, -3 y -8, el resultado será -4.333333333333333 */
+/* Con los valores -1.2, 5.3 y -3.3, el resultado será 0.2666666666666666 */

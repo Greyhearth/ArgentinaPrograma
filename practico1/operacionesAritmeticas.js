808b299860b3cfefed186c2a63123c6a8df95bf4
Gustavo <gustavo.grisetti@mi.unc.edu.ar>
2023-06-06 21:29:09 -0300

    Agrego el archivo operacionesAritmeticas.js

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,10 @@
+let var1 = 12;
+let var2 = 10;
+let var3 = 4;
+let var4 = 2;
+let sumaVar = var1+var2;
+let restaVar = var3-var4;
+let multipliVar = sumaVar*restaVar;
+let esPar = multipliVar % 2 === 0;
+
+console.log("Mis variables iniciales fueron: "+var1+", "+var2+", "+var3+" y "+var4+". La respuesta a verificar si el resultado final es par es: "+esPar);

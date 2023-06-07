da5ce2c14077d7351747cea13d149b62a90381c0
Gustavo <gustavo.grisetti@mi.unc.edu.ar>
2023-06-06 22:15:45 -0300

    Realizo correcciones en miNombreCompleto.js

--- practico1/miNombreCompleto.js
+++ practico1/miNombreCompleto.js
@@ -1,6 +1,15 @@
+// 1. Escribir un programa que:
+//a. Defina una variable nombre.
 let nombre;
+
+//b. Guarde en la variable nombre tu nombre real.
 nombre = "Gustavo";
+
+//c. Defina y asigne, en un mismo paso, la variable apellido. El valor a asignar deberá ser tu apellido real.
 let apellido = "Grisetti";
+
+//d. Repita el paso anterior, pero esta vez definiendo la variable edad y asignandole tu edad real.
 let edad = 30;
 
+//e. Por último, imprime por consola un mensaje que respete el siguiente formato:
 console.log("Hola, "+nombre+" "+apellido+". Tienes "+edad+" años.");

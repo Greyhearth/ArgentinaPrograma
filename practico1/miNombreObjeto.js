e475c5c175bcb63dc72fc3c21d38de658f1d2e26
Gustavo <gustavo.grisetti@mi.unc.edu.ar>
2023-06-06 21:18:07 -0300

    Agrego el archivo miNombreObjeto.js

---
+++ practico1/miNombreObjeto.js
@@ -0,0 +1,4 @@
+let persona = {nombre: "Gustavo", apellido: "Grisetti", edad: 30};
+
+console.log("Mi objeto persona es el siguiente: "+ JSON.stringify(persona) )
+//revisar la función JSON.stringify

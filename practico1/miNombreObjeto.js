39b48d3a1f172ff42dfb21bd9b1c745c31052e49
Gustavo <gustavo.grisetti@mi.unc.edu.ar>
2023-06-06 22:17:58 -0300

    Realizo correcciones en miNombreObjeto.js

--- practico1/miNombreObjeto.js
+++ practico1/miNombreObjeto.js
@@ -1,4 +1,7 @@
+//2. ¿De qué manera representaría las variables del ejercicio anterior en un objeto, llamémosle persona?
+//a. Defina una variable de tipo objeto que lleve el nombre persona, y que contenga las variables nombre, apellido y edad del ejercicio anterior con sus respectivos valores.
 let persona = {nombre: "Gustavo", apellido: "Grisetti", edad: 30};
 
+//b. Imprime por consola un mensaje que respete el siguiente formato:
 console.log("Mi objeto persona es el siguiente: "+ JSON.stringify(persona));
 //revisar la función JSON.stringify

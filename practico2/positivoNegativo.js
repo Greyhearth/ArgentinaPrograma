e94dc6e5caae18137318044bce535b453f78425a
Gustavo <gustavo.grisetti@mi.unc.edu.ar>
2023-06-17 10:15:06 -0300

    Revisado el archivo positivoNegativo.js

--- practico2/positivoNegativo.js
+++ practico2/positivoNegativo.js
@@ -3,7 +3,7 @@ const readlineSync = require('readline-sync');
 let numero = readlineSync.question("Ingrese un numero: ");
 if (numero>0){
     console.log("El numero es positivo");
-}else if (numero=0){
+}else if (numero==0){
     console.log("El numero es cero");
 }else if (numero<0){
     console.log("El numero es negativo");

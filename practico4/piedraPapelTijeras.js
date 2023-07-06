3ebb86ac769f28e5266e9e954f228f78c2ad768d
Gustavo <gustavo.grisetti@mi.unc.edu.ar>
2023-07-06 19:19:58 -0300

    Agrego el archivo piedraPapelTijeras.js con el primer ejercicio extra resuelto

--- practico4/piedraPapelTijeras.js
+++ practico4/piedraPapelTijeras.js
@@ -2,7 +2,7 @@
     - 0 para "piedra",
     - 1 para "papel" y
     - 2 para "tijeras"
-Siempre utilizando los valores de strings y no los valores númericos.*/
+Siempre utilizando los valores de strings y no los valores númericos.
 let jugadaComputadora;
 let jugadaUsuario;
 let resultado;
@@ -23,18 +23,18 @@ function obtenerJugadaComputadora () {
     return jugadaComputadora      
 }
 
-/*3. Crea una función llamada obtenerJugadaUsuario que solicitará al usuario ingresar su elección a través de la consola.
-Los valores posibles son: piedra, papel o tijeras.*/
+3. Crea una función llamada obtenerJugadaUsuario que solicitará al usuario ingresar su elección a través de la consola.
+Los valores posibles son: piedra, papel o tijeras.
 const readlineSync = require('readline-sync');
 
 function obtenerJugadaUsuario () {
-    let eleccion = readlineSync.question("Elija su jugada:\n   piedra\n   papel\n   tijeras\n");
+    let eleccion = readlineSync.question("Elija su jugada. Los valores posibles son: piedra, papel o tijeras\n");
     jugadaUsuario = eleccion
     return jugadaUsuario
 }
 
-/*4. Crea una función llamada determinarGanador que tomará como parámetros las jugadas de la computadora y del usuario. 
-Implementa las reglas del juego para determinar el ganador y retorna el resultado.*/
+4. Crea una función llamada determinarGanador que tomará como parámetros las jugadas de la computadora y del usuario. 
+Implementa las reglas del juego para determinar el ganador y retorna el resultado.
 function determinarGanador (jugada1, jugada2) {
     if (jugada1 == jugada2){
         return resultado = "Empate"
@@ -44,21 +44,60 @@ function determinarGanador (jugada1, jugada2) {
             return resultado = "Gana el usuario"
     }
 }
-/*5. Llama a las funciones en el orden adecuado para ejecutar el juego:
+5. Llama a las funciones en el orden adecuado para ejecutar el juego:
     a) Invoca la función obtenerJugadaComputadora y almacena el resultado en una variable.
     b) Invoca la función obtenerJugadaUsuario y almacena el resultado en otra variable.
-    c) Invoca la función determinarGanador pasando como argumentos las jugadas de la computadora y del usuario. Almacena el resultado en una variable.*/
+    c) Invoca la función determinarGanador pasando como argumentos las jugadas de la computadora y del usuario. Almacena el resultado en una variable.
 obtenerJugadaComputadora ();
 obtenerJugadaUsuario ();
 determinarGanador (jugadaUsuario, jugadaComputadora);
 
-/*6. Imprime por pantalla el resultado del juego utilizando la función console.log(). El mensaje debe indicar la jugada de la computadora, la jugada del usuario y 
-el resultado del juego (quién ganó o si fue un empate). Se debe respetar el siguiente formato dependiendo del resultado:*/
+6. Imprime por pantalla el resultado del juego utilizando la función console.log(). El mensaje debe indicar la jugada de la computadora, la jugada del usuario y 
+el resultado del juego (quién ganó o si fue un empate). Se debe respetar el siguiente formato dependiendo del resultado:
 console.log("La computadora eligio: %s.", jugadaComputadora);
 console.log("El usuario eligio: %s.", jugadaUsuario);
 console.log("El resultado fue: %s.", resultado);
 
-/*EXTRA 1: Definir constantes para las distintas jugadas, es decir, piedra, papel y tijera y adaptar el código para su correcta utilización.*/
+EXTRA 1: Definir constantes para las distintas jugadas, es decir, piedra, papel y tijera y adaptar el código para su correcta utilización.*/
+const readlineSync = require('readline-sync');
+const jugadas = ["piedra", "papel", "tijeras"];
+let jugadaComputadora;
+let jugadaUsuario;
+let resultado;
+let eleccion;
+
+function obtenerJugadaComputadora () {
+    jugadaComputadora = jugadas[Math.floor(Math.random() * 3)];
+    return jugadaComputadora;      
+}
+
+function obtenerJugadaUsuario () {
+    eleccion = readlineSync.question("Elija su jugada. Los valores posibles son: piedra, papel o tijeras\n");
+    while (!(jugadas.includes(eleccion.toLowerCase()))){
+        console.clear();
+        eleccion = readlineSync.question("Jugada invalida. Vuelva a elegir su jugada.\nLos valores posibles son: piedra, papel o tijeras\n");
+    }
+    jugadaUsuario = eleccion.toLowerCase();
+    return jugadaUsuario;
+}
+
+function determinarGanador (jugada1, jugada2) {
+    if (jugada1 == jugada2){
+        return resultado = "Empate"
+    } else if ((jugada1 == jugadas[0] && jugada2 == jugadas[1]) || (jugada1 == jugadas[1] && jugada2 == jugadas[2]) || (jugada1 == jugadas[2] && jugada2 == jugadas[0])) {
+            return resultado = "Gana la computadora"
+    } else if ((jugada1 == jugadas[0] && jugada2 == jugadas[2]) || (jugada1 == jugadas[1] && jugada2 == jugadas[0]) || (jugada1 == jugadas[2] && jugada2 == jugadas[1])) {
+            return resultado = "Gana el usuario"
+    }
+}
+
+obtenerJugadaComputadora ();
+obtenerJugadaUsuario ();
+determinarGanador (jugadaUsuario, jugadaComputadora);
+
+console.log("La computadora eligio: %s.", jugadaComputadora);
+console.log("El usuario eligio: %s.", jugadaUsuario);
+console.log("El resultado fue: %s.", resultado);
 
 /*EXTRA 2: Agregar la posibilidad de que el usuario elija en cuantos jugadas quiere terminar el juego, es decir:
     Una jugada: gana el ganador de esa única jugada.
